07be050c9e0cf3c3e5e906530043fb04893d5c78
James A. Rosen <james@zendesk.com>
2011-12-16 20:04:17 -0800

    use Dir[] for requiriing everything

--- lib/rake-pipeline-web-filters.rb
+++ lib/rake-pipeline-web-filters.rb
@@ -9,11 +9,7 @@ module Rake
   end
 end
 
-require "rake-pipeline-web-filters/version"
-require "rake-pipeline-web-filters/tilt_filter"
-require "rake-pipeline-web-filters/sass_compiler"
-require "rake-pipeline-web-filters/minispade_filter"
-require "rake-pipeline-web-filters/ordering_concat_filter"
-require "rake-pipeline-web-filters/markdown_compiler"
-require "rake-pipeline-web-filters/cache_buster"
+Dir[File.expand_path('../rake-pipeline-web-filters/**/*.rb', __FILE__)].each do |f|
+  require f
+end
 
