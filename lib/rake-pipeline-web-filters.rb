fadf9004a7604fce2c1f8368e51155279a62bbee
Yehuda Katz <wycats@gmail.com>
2011-12-18 22:47:02 -0800

    Let's not do this, mmmkay

--- lib/rake-pipeline-web-filters.rb
+++ lib/rake-pipeline-web-filters.rb
@@ -9,7 +9,13 @@ module Rake
   end
 end
 
-Dir[File.expand_path('../rake-pipeline-web-filters/**/*.rb', __FILE__)].each do |f|
-  require f
-end
+require "rake-pipeline-web-filters/version"
+require "rake-pipeline-web-filters/cache_buster"
+require "rake-pipeline-web-filters/filter_with_dependencies"
+require "rake-pipeline-web-filters/markdown_compiler"
+require "rake-pipeline-web-filters/minispade_filter"
+require "rake-pipeline-web-filters/ordering_concat_filter"
+require "rake-pipeline-web-filters/sass_compiler"
+require "rake-pipeline-web-filters/tilt_filter"
+require "rake-pipeline-web-filters/helpers"
 
